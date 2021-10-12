bcb0edf83dc143b5dbbce56ca211a3d58c417e8e
宇豪 林 <1005772685@qq.com>
2021-10-12 23:19:14 +0800

    添加文件qwq

---
+++ HelloGit/src/main/java/zhuluo/HelloGit.java
@@ -0,0 +1,10 @@
+package zhuluo;
+
+import org.graalvm.compiler.serviceprovider.IsolateUtil;
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
