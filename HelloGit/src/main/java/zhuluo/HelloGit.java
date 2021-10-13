592cd0be7c20ce4d0216942c619a52b1a268e231
宇豪 林 <1005772685@qq.com>
2021-10-13 17:52:51 +0800

    更新1

--- HelloGit/src/main/java/zhuluo/HelloGit.java
+++ HelloGit/src/main/java/zhuluo/HelloGit.java
@@ -6,5 +6,6 @@ import org.w3c.dom.ls.LSOutput;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("更新1");
     }
 }
